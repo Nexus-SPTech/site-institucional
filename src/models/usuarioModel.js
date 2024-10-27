c0994f4b252f1032f4e9cc2215b28754e8ecccdf
roniandradee <ronielli.santos@sptech.school>
2024-10-27 15:07:58 -0300

    alterando post

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -16,7 +16,7 @@ function cadastrar(nome, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+        INSERT INTO usuario (nomeUsuario, email, senha) VALUES ('${nome}', '${email}', '${senha}');
     `;
     return database.executar(instrucaoSql);
 }
