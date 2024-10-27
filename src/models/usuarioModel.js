7e8d417d0bcd2c10b19d7dc6b31e238c377864b2
roniandradee <ronielli.santos@sptech.school>
2024-10-27 15:37:11 -0300

    Update usuarioModel.js

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -4,7 +4,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nomeUsuario, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     return database.executar(instrucaoSql);
 }
